12da13575ade820477df841c42e70b3303867705
“HANIBA2752” <wuttipat2004@gmail.com>
2025-01-08 22:08:21 +0700

    lab3

---
+++ part1/README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# engce301_2-67
+=======
+# PART1
+Reactjs and Nodejs
+>>>>>>> 0cb474f (LAB_02)
---
+++ part1/back-end/dbconfig.js
@@ -0,0 +1,19 @@
+var dbconfig = {
+    development: {
+        //connectionLimit : 10,
+        host     : '10.0.97.89',
+        port     : '3306',
+        user     : 'root',
+        password : '',
+        database : 'moviedb'
+    },
+    production: {
+        //connectionLimit : 10,
+        host     : '10.0.97.89',
+        port     : '3306',
+        user     : 'root',
+        password : '',
+        database : 'moviedb'
+    }
+};
+module.exports = dbconfig;
